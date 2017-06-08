cbaff0994c901e22532d1df9a6c8a211709e6c17
Dusty McCord <dustymccord@gmail.com>
2017-06-07 22:13:15 -0700

    added smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
+const menu = function() {
+    var topNav= document.getElementById("myTopnav");
+    if (topNav.className === "topnav") {
+        topNav.className += " responsive";
     } else {
-        x.className = "topnav";
+        topNav.className = "topnav";
     }
 }
+
+$(document).ready(function() {
+  $("a").on('click', function(event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        event.preventDefault();
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function(){
+
+          window.location.hash = hash;
+        });
+      } // End if
+    });// on click
+
+}); //end doc ready
